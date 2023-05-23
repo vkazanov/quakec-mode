684e8e0ea37cb61eda18286780e44f0cfc2a7b7c
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2023-05-23 15:44:28 +0100

    Add a quick find-defintions test

--- test/quakec-mode-test.el
+++ test/quakec-mode-test.el
@@ -1,3 +1,29 @@
 ;;; quakec-mode-test.el --- Tests for quakec-mode
+(require 'quakec-mode)
+
+(ert-deftest find-definitions-test ()
+  (with-quakec-temp-buffer
+      "
+/* a random multiline comment*/
+
+/* a commented out declaration*/
+/* void() Declared; */
+
+// void() Declared;
+
+/* a declaration */
+void() Declared;
+
+/* a definition */
+float(entity targ, entity inflictor) Declared =
+{
+    a = b
+}
+
+"
+    (let ((defs (quakec--find-file-definitions "Declared")))
+      (should (equal (length defs) 2))
+      (should (equal (xref-item-summary (nth 0 defs)) "Declared"))
+      (should (equal (xref-item-summary (nth 1 defs)) "Declared")))))
 
 ;;; quakec-mode-test.el ends here
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,11 @@
 ;;; test-helper.el --- Helpers for quakec-mode-test.el
 
+(defmacro with-quakec-temp-buffer (contents &rest body)
+  "Create a temporary buffer to execute a test in."
+  (declare (indent 1) (debug t))
+  `(with-temp-buffer
+     (insert ,contents)
+     (goto-char (point-min))
+     ,@body))
+
 ;;; test-helper.el ends here
